6e296ac4f54017ff28e6d82957aaffcfba3b05c1
Sibiraj <dev.sibiraj@outlook.com>
2018-07-28 02:24:28 +0530

    chore: mark repo as private to prevent accidental publish

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -10,6 +10,7 @@ fs.readFile('dist/package.json', 'utf8', function (err, data) {
     pkg['main'] = 'angularjs-toast.min.js';
     delete pkg['scripts'];
     delete pkg['devDependencies'];
+    delete pkg['private'];
 
     fs.writeFile('dist/package.json', JSON.stringify((pkg), null, 2), 'utf8', function (err) {
         if (err) throw err;
